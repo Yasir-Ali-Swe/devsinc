0af6b40b9bce16441be80c7c4fd1fa5f1705d0af
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2025-07-26 15:05:25 +0500

    Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,13 +3,12 @@ import React from 'react';
 const Hero = () => {
     return (
         <div className='h-screen relative overflow-hidden'>
-            {/* Background Video */}
             <video
                 autoPlay
                 loop
                 muted
                 playsInline
-                className='w-screen h-[93%] object-cover'
+                className='h-[70%] w-screen lg:h-[100%] object-cover'
             >
                 <source
                     src="https://cdn.prod.website-files.com/6719ad0ceed6d5aa24a83d61/67988ed24841623ccd9affd6_homepage%20global%20(1)-transcode.webm"
@@ -17,23 +16,18 @@ const Hero = () => {
                 />
                 Your browser does not support the video tag.
             </video>
-
-            {/* Overlay with light black color */}
-            <div className='absolute top-0 left-0 w-full h-[93%] bg-black opacity-40 z-20'></div>
-
-            {/* Let's Talk Business button - rotated and aligned to right edge */}
-            <button className="fixed top-1/2 right-[-65px] -translate-y-1/2 rotate-90 z-50 px-6 py-3 cursor-pointer text-black rounded-xl bg-primary">
+            <div className='absolute top-0 left-0 w-full h-[70%] lg:h-[100%] bg-black opacity-70 z-20'></div>
+            <button className="fixed top-1/2 right-[-55px] lg:right-[-65px] lg:-translate-y-1/2 rotate-90 z-50 px-3 lg:px-6 py-2  lg:py-3 cursor-pointer text-black rounded-xl bg-primary">
                 Let's Talk Business
             </button>
-
-
-            {/* Text content */}
-            <div className='absolute top-1/3 left-20 z-40 text-white'>
-                <h1 className='text-6xl font-bold mb-3'>Drive Tomorrow’s</h1>
-                <h1 className='text-6xl font-bold mb-6'>Possibilities</h1>
-                <h1 className='text-2xl mb-2'>We help companies redefine the future</h1>
-                <h1 className='text-2xl mb-6'>through technology</h1>
-                <button className='mt-4 px-6 py-3 cursor-pointer text-white rounded-3xl bg-primary'>
+            <div className='absolute top-[25%] left-6 lg:top-1/3 lg:left-20 z-40 text-white'>
+                <h1 className='text-4xl lg:text-6xl font-bold mb-1 lg:mb-3'>Drive Tomorrow’s</h1>
+                <h1 className='text-4xl lg:text-6xl font-bold mb-2 lg:mb-6'>Possibilities</h1>
+                <h1 className='text-xl lg:text-2xl lg:mb-2'>We help companies redefine the future
+                    <span className='lg:hidden'> through technology</span>
+                </h1>
+                <h1 className='hidden lg:block lg:text-2xl lg:mb-6'>through technology</h1>
+                <button className='px-3 py-2 mt-2 lg:mt-4 lg:px-6 lg:py-3 cursor-pointer text-white rounded-3xl bg-primary'>
                     Get in Touch
                 </button>
             </div>

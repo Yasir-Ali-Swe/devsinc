0ce06b2a25305a5f75ddc724bd41442d4d225c37
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2025-07-28 22:32:05 +0500

    UsersProfile.jsx

--- src/components/UsersProfile.jsx
+++ src/components/UsersProfile.jsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useRef } from 'react';
 import { FaArrowRight, FaArrowLeft, FaLinkedin } from "react-icons/fa6";
+import { useTheme } from '@/context/ThemeContext';
 
 const images = [
     { id: 1, src: "UsersProfile/101.png", firstname: "Usman", lastname: "Asif", title: "Founder, CEO" },
@@ -12,6 +13,7 @@ const images = [
 ];
 
 const UsersProfile = () => {
+    const { theme } = useTheme();
     const scrollRef = useRef(null);
 
     const scrollLeft = () => {
@@ -23,7 +25,7 @@ const UsersProfile = () => {
     };
 
     return (
-        <div className='py-10 px-10 md:px-20 md:py-20'>
+        <div className={`transition-all duration-1000 py-10 px-10 md:px-20 md:py-20 ${theme ? 'bg-black' : 'bg-white'}`}>
             <div className='flex justify-between items-center'>
                 <h1 className='text-3xl bg-gradient-to-r from-[#0c4764] to-[#0da7a4] bg-clip-text text-transparent leading-12 md:mt-8'>Our Global Leadership</h1>
                 <div className='flex items-center gap-5'>
@@ -47,7 +49,7 @@ const UsersProfile = () => {
                 className='py-5 md:pl-100 lg:pl-90 flex flex-row gap-6 overflow-x-auto scroll-smooth scrollbar-hide'
             >
                 {images.map((item) => (
-                    <div key={item.id} className='min-w-[360px] md:min-w-[400px] lg:min-w-[600px] bg-white p-4'>
+                    <div key={item.id} className={`min-w-[360px] md:min-w-[400px] lg:min-w-[600px] ${theme ? 'bg-black text-white' : 'bg-white text-black'} p-4`}>
                         <div>
                             <img src={item.src} alt="UserImg" className='w-full' />
                         </div>
@@ -55,7 +57,7 @@ const UsersProfile = () => {
                             {item.firstname} <span className='text-primary'>{item.lastname}</span>
                         </h1>
                         <h2 className='text-lg font-light pb-2'>{item.title}</h2>
-                        <FaLinkedin size={30} className='text-blue-600' />
+                        <FaLinkedin size={30} className={`${theme ? 'text-white' : 'text-black'}`} />
                     </div>
                 ))}
             </div>

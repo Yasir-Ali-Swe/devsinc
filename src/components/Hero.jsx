a13ecceb17c84341e2ca48c7cb1e382cf1291b17
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2025-07-28 22:29:28 +0500

    Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,25 @@
+"use client";
 import React from 'react';
+import { motion } from "framer-motion";
+
+const containerAnimation = {
+    hidden: {},
+    visible: {
+        transition: {
+            staggerChildren: 0.5
+        }
+    }
+};
+
+const textAnimation = {
+    hidden: { y: 100, opacity: 0 },
+    visible: {
+        y: 0,
+        opacity: 1,
+        transition: { duration: 0.7 }
+    }
+};
+
 
 const Hero = () => {
     return (
@@ -8,7 +29,7 @@ const Hero = () => {
                 loop
                 muted
                 playsInline
-                className='h-[70%] w-screen lg:h-[100%] object-cover'
+                className='h-screen w-screen lg:h-[100%] object-cover'
             >
                 <source
                     src="https://cdn.prod.website-files.com/6719ad0ceed6d5aa24a83d61/67988ed24841623ccd9affd6_homepage%20global%20(1)-transcode.webm"
@@ -16,21 +37,40 @@ const Hero = () => {
                 />
                 Your browser does not support the video tag.
             </video>
-            <div className='absolute top-0 left-0 w-full h-[70%] lg:h-[100%] bg-black opacity-70 z-20'></div>
-            <button className="fixed top-1/2 right-[-55px] lg:right-[-65px] lg:-translate-y-1/2 rotate-90 z-50 px-3 lg:px-6 py-2  lg:py-3 cursor-pointer text-black rounded-xl bg-primary">
+            <div className='absolute top-0 left-0 w-full h-[100%] lg:h-[100%] bg-black opacity-70 z-20'></div>
+            <button  className="fixed top-1/2 right-[-55px] lg:right-[-65px] lg:-translate-y-1/2 rotate-90 z-50 px-3 lg:px-6 py-2 lg:py-3 cursor-pointer text-black rounded-xl bg-primary">
                 Let's Talk Business
             </button>
-            <div className='absolute top-[23%] md:top-[10%] left-6 lg:top-1/3 lg:left-20 z-40 text-white'>
-                <h1 className='text-4xl lg:text-6xl font-bold mb-1 lg:mb-3'>Drive Tomorrow’s</h1>
-                <h1 className='text-4xl lg:text-6xl font-bold mb-2 lg:mb-6'>Possibilities</h1>
-                <h1 className='text-2xl font-thin lg:text-4xl lg:mb-2 mt-5'>We help companies redefine the future
-                    <span className='lg:hidden text-2xl font-thin lg:text-4xl lg:mb-2'> through technology</span>
-                </h1>
-                <h1 className='hidden lg:block lg:text-2xl lg:mb-6'>through technology</h1>
-                <button className='px-6 py-4 md:py-6 text-2xl mt-6  lg:mt-6 lg:px-6 lg:py-3 cursor-pointer text-white rounded-4xl bg-primary'>
+            <motion.div
+                variants={containerAnimation}
+                initial="hidden"
+                animate="visible"
+                className='absolute top-[40%] md:top-[10%] left-6 lg:top-1/3 lg:left-20 z-40 text-white'
+            >
+                <motion.h1 variants={textAnimation} className='text-4xl lg:text-6xl font-bold mb-1 lg:mb-3'>
+                    Drive Tomorrow’s
+                </motion.h1>
+
+                <motion.h1 variants={textAnimation} className='text-4xl lg:text-6xl font-bold mb-2 lg:mb-6'>
+                    Possibilities
+                </motion.h1>
+
+                <motion.h1 variants={textAnimation} className='text-2xl font-thin lg:text-4xl lg:mb-2 mt-5'>
+                    We help companies redefine the future
+                    <span className='lg:hidden'> through technology</span>
+                </motion.h1>
+
+                <motion.h1 variants={textAnimation} className='hidden lg:block lg:text-2xl lg:mb-6'>
+                    through technology
+                </motion.h1>
+
+                <motion.button
+                    variants={textAnimation}
+                    className='px-6 py-4 md:py-6 text-2xl mt-6 lg:mt-6 lg:px-6 lg:py-3 cursor-pointer text-white rounded-4xl bg-primary'
+                >
                     Get in Touch
-                </button>
-            </div>
+                </motion.button>
+            </motion.div>
         </div>
     );
 };

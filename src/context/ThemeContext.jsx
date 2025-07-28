3efee017ec30efc8ee43823c33b980010a03c09c
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2025-07-28 22:32:33 +0500

    Theme Toggle

---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,24 @@
+'use client';
+import { createContext, useContext, useState, useEffect } from 'react';
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState(true); // true = dark
+
+  useEffect(() => {
+    if (theme) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
